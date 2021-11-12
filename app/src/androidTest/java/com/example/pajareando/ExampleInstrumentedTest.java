5452fa752e986387964606ad0f66503b723809fc
FabianAponte <apontenator@gmail.com>
2021-11-12 18:51:18 -0500

    Ok cambio01

--- app/src/androidTest/java/com/example/pajareando/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pajareando/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication01;
+package com.example.pajareando;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication01", appContext.getPackageName());
+        assertEquals("com.example.pajareando", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/pajareando/LoginActivity.java
+++ app/src/main/java/com/example/pajareando/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication01;
+package com.example.pajareando;
 
 import androidx.appcompat.app.AppCompatActivity;
 
