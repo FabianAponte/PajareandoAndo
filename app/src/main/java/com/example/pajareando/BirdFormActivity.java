f7e8d775009570cb1b4f52608bc95d98e485d36f
Andres Castrillon <andres.castrillon@alegra.com>
2021-11-14 20:39:18 -0500

    se agrega logica para la foto

--- app/src/main/java/com/example/pajareando/BirdFormActivity.java
+++ app/src/main/java/com/example/pajareando/BirdFormActivity.java
@@ -3,10 +3,14 @@ package com.example.pajareando;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class BirdFormActivity extends AppCompatActivity {
 
@@ -16,11 +20,22 @@ public class BirdFormActivity extends AppCompatActivity {
     final String []birdTypes =  {"Tipo 1", "Tipo 2", "Tipo 3"};
     final String []birdSize =  {"Pequeño", "Mediano", "Grande"};
 
+    boolean hasImage = false;
+    Button registerButton;
+    ImageView birdImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bird_form);
 
+        registerButton = findViewById(R.id.registerButton);
+        birdImage = findViewById(R.id.birdImage);
+
+        startCombos();
+    }
+
+    private void startCombos() {
         autoCompleteBirdTypeView = findViewById(R.id.autoCompleteBirdType);
         autoCompleteBirdSizeView = findViewById(R.id.autoCompleteBirdSize);
 
@@ -36,5 +51,27 @@ public class BirdFormActivity extends AppCompatActivity {
     public void goToMain(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
+    }
+
+    public void register(View view) {
+        if (!hasImage) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(intent, 1);
+            }
+        } else {
+            this.goToMain(view);
+        }
+    }
+
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            birdImage.setImageBitmap(imgBitmap);
+            hasImage = true;
+        }
     }
 }
